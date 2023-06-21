dd99d9b28148e7bc8cd91a1f913b861220505ab6
Sayan Biswas <sayanbiswas6073@gmail.com>
2023-06-21 22:55:06 +0530

    bug fixing

--- src/components/Navsidebar.jsx
+++ src/components/Navsidebar.jsx
@@ -19,7 +19,7 @@ function Navsidebar({click,cart}) {
 
   useEffect(()=>{
     if(loggdin){
-      axios.get("/api/getCart/route").then(res=>{
+      axios.get("/api/getcart").then(res=>{
         console.log(res)
         if(res.status == 200){
           //dispatch(setCart(res.data))
