4dc6ee90d19270a3ddf8223427cd200d673faa60
Sayan Biswas <sayanbiswas6073@gmail.com>
2023-06-21 20:53:49 +0530

    fixing cors problem

--- middleware.js
+++ middleware.js
@@ -3,10 +3,23 @@ import { NextResponse } from "next/server";
 
 
 export function middleware(request) {
-    const response = NextResponse.next()
-
-    if (request.nextUrl.pathname.startsWith("/api")) {
-        response.headers.append("Access-Control-Allow-Origin", "*")
-      }
-      return response
+    const allowedOrigins = [
+        "https://candyhub.varcel.app"
+    ];
+        
+    const requestHeaders = new Headers(request.headers);
+    const response = NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    });
+    
+    const origin = requestHeaders.get('origin');
+    if ( origin && allowedOrigins.includes(origin) ) {
+      response.headers.set('Access-Control-Allow-Origin', origin)
+      response.headers.set('Access-Control-Allow-Credentials', "true")
+      response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    }
+    
+    return response
 }
